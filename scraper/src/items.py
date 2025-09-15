64a99b2bdb320e0bb0a43194272900bb0354f63e
Rafael Italiano <rafaeljitaliano@gmail.com>
2025-09-14 21:01:43 -0300

    add Containerfile to project

--- scraper/src/items.py
+++ scraper/src/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class HardhatItem(scrapy.Item):
+class ConstructionMaterialItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
