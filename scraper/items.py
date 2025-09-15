d3a9d730e6a551de8593f699485bee2085c89396
Claudiane Amaral <79753765+claudiane-art@users.noreply.github.com>
2025-09-14 21:04:10 -0300

    Revert "add Containerfile to project"

--- scraper/items.py
+++ scraper/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class ConstructionMaterialItem(scrapy.Item):
+class HardhatItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
