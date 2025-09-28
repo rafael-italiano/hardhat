2305d18d8d90d6be5a3cd06815d522b12f118849
Rafael Italiano <rafaeljitaliano@gmail.com>
2025-09-28 14:37:38 -0300

    add product service

---
+++ scraper/src/services/categories_service.py
@@ -0,0 +1,23 @@
+from scrapy.crawler import CrawlerProcess
+
+from services.base_service import BaseService
+from spiders.categories_spider import LeroyMerlinCategoriesSpider
+
+class LeroyMerlinCategoryService(BaseService):
+    def __init__(self) -> None:
+        self.results = []
+
+    def process_item(self, item) -> None:
+        
+        self.results.append(item)
+        return None
+    
+    def process(self) -> list:
+
+        process = CrawlerProcess()
+        process.crawl(
+            LeroyMerlinCategoriesSpider,
+            item_callback=self.process_item
+        )
+        process.start()
+        return self.results
