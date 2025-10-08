747f20021c3ed173122d3716775e4ee5395b4f8e
claudiane-art <claudiane.silvaamaral.com>
2025-10-07 22:55:24 -0300

    Iniciado criação do ML

---
+++ price-ml/training.sql
@@ -0,0 +1,24 @@
+WITH daily_avg AS (
+    SELECT
+        products.external_id,
+        prices.created_at::DATE AS price_date,
+        AVG(prices.price) AS daily_average_price,
+        subcategory_id
+    FROM products
+    LEFT JOIN prices ON products.id = prices.product_id
+    GROUP BY
+        subcategory_id,
+        products.external_id,
+        prices.created_at::DATE
+),
+previous_average as (SELECT
+    external_id,
+    price_date,
+    daily_average_price,
+    LAG(daily_average_price, 1, NULL) OVER (PARTITION BY external_id ORDER BY price_date) AS previous_day_average_price,
+    subcategory_id
+FROM daily_avg)
+
+select *
+ from previous_average
+ where previous_day_average_price is not null;
