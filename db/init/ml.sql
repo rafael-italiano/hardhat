2063d0ec9d65d7e93deb0e261ed941d09aefe819
Rafael Italiano <rafaeljitaliano@gmail.com>
2025-10-09 01:43:55 -0300

    finish component

--- db/init/ml.sql
+++ db/init/ml.sql
@@ -3,14 +3,15 @@ CREATE SCHEMA ml
 CREATE TABLE ml.model (
     id SERIAL PRIMARY KEY,
     mse NUMERIC(12,2) NOT NULL,
-    r2 NUMERIC(12,2) NOT NULL,
+    r2 NUMERIC(12,10) NOT NULL,
     created_at TIMESTAMP DEFAULT now() NOT NULL
 );
 
-CREATE TABLE ml.predict_prices (
+CREATE TABLE ml.predicted_price (
     id SERIAL PRIMARY KEY,
     model_id INTEGER REFERENCES ml.model(id) ON DELETE SET NULL, 
-    price_id NUMERIC(12,2) NOT NULL,
-    date NUMERIC(12,2) NOT NULL,
-    predicted_price TIMESTAMP DEFAULT now() NOT NULL
+    external_product_id NUMERIC(12,2) NOT NULL,
+    prediction_date DATE NOT NULL,
+    predicted_price NUMERIC(12,2) NOT NULL,
+    created_at TIMESTAMP DEFAULT now() NOT NULL
 );
